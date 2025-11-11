278f838f3de80ecef9445507ba1ea0d413187fe1
Derek Knowlton <knowltod@oregonstate.edu>
2025-11-10 18:11:11 -0800

    added react-leaflet library and change map to a map component. Added a marker with popup information when clicked

--- frontend/src/Components/Map/Map.tsx
+++ frontend/src/Components/Map/Map.tsx
@@ -1,22 +1,39 @@
-import { useEffect, useRef } from "react";
 import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 function Map() {
-  const map = useRef<L.Map | null>(null);
-
-  useEffect(() => {
-    if (!map.current) {
-      map.current = L.map("mapId", {
-        preferCanvas: true,
-      }).setView([44.5646, -123.262], 6);
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: "&copy; OpenStreetMap contributors",
-      }).addTo(map.current);
-    }
-  });
-
-  return <div id="mapId" style={{ height: "100vh", width: "100vw" }}></div>;
+  return (
+    <MapContainer
+      center={[44.5646, -123.262]}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: "100vh", width: "100vw" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[44.5646, -123.262]}>
+        <Popup>
+          // TODO: <br />
+          Insert Node Data Here
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
 
 export default Map;
