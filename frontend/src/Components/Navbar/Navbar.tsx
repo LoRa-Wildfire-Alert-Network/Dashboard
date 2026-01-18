14f7fa2700b9fc26f544dc83db10d4f6adcf93a8
Derek Knowlton <knowltod@oregonstate.edu>
2026-01-17 21:28:19 -0800

    added text to navbar and spaced accordingly

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,7 +1,10 @@
 const Navbar = () => {
   return (
-    <div className="flex w-full h-16 bg-slate-600">
-      <div></div>
+    <div className="flex justify-between items-center w-full h-16 bg-slate-600">
+      <p className="text-white text-lg font-semibold p-6">
+        LoRa Wildfire Dashboard
+      </p>
+      <p className="text-white text-lg font-semibold p-6">Login</p>
     </div>
   );
 };
