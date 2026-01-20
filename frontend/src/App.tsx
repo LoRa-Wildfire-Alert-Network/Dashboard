f636ddb7f2d3c33b577ef28d7d260bee2eb44f53
Derek M. Knowlton <97196662+knowltod@users.noreply.github.com>
2026-01-20 06:19:04 -0800

    Update frontend/src/App.tsx
    Co-authored-by: Derek Greene <derekgreene11@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -93,8 +93,6 @@ function App() {
       <div className="bg-slate-300 h-[calc(100vh-4rem)]">
         <div className="flex space-x-4 w-full h-full p-4">
           <div className="flex-none lg:w-80 md:w-48 bg-slate-200 rounded-md p-4">
-            <h2 className="text-xl font-semibold mb-2">Column 1</h2>
-            <p>First Column</p>
           </div>
           <Map nodeData={testData} />
           <div className="flex-none overflow-y-auto lg:w-100 md:w-60 bg-slate-400 rounded-md py-2 px-4">
