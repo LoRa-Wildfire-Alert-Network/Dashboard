6d30c602baa10e6a80bc7c8e6a178f9754ccb9be
Derek Greene <derek@derekrgreene.com>
2026-01-22 20:23:11 -0800

    :lipstick: Add: Sign-in/sign-up components and welcome greeting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
 import Map from "./Components/Map/Map";
 import Navbar from "./Components/Navbar/Navbar";
 import NodeCard from "./Components/NodeCard/NodeCard";
@@ -90,20 +91,30 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="bg-slate-300 h-[calc(100vh-4rem)]">
-        <div className="flex space-x-4 w-full h-full p-4">
-          <div className="flex-none lg:w-80 md:w-48 bg-slate-200 rounded-md p-4">
-            <h2 className="text-xl font-semibold mb-2">Column 1</h2>
-            <p>First Column</p>
-          </div>
-          <Map nodeData={testData} />
-          <div className="flex-none overflow-y-auto lg:w-100 md:w-60 bg-slate-400 rounded-md py-2 px-4">
-            {testData.map((nodeData, i) => (
-              <NodeCard key={i} nodeData={nodeData} />
-            ))}
+      <SignedIn>
+        <div className="bg-slate-300 h-[calc(100vh-4rem)]">
+          <div className="flex space-x-4 w-full h-full p-4">
+            <div className="flex-none lg:w-80 md:w-48 bg-slate-200 rounded-md p-4">
+              <h2 className="text-xl font-semibold mb-2">Column 1</h2>
+              <p>First Column</p>
+            </div>
+            <Map nodeData={testData} />
+            <div className="flex-none overflow-y-auto lg:w-100 md:w-60 bg-slate-400 rounded-md py-2 px-4">
+              {testData.map((nodeData, i) => (
+                <NodeCard key={i} nodeData={nodeData} />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+      </SignedIn>
+      <SignedOut>
+        <div className="bg-slate-300 h-[calc(100vh-4rem)] flex items-center justify-center">
+          <div className="text-center">
+            <h1 className="text-2xl font-semibold mb-4">Welcome to LoRa Wildfire Dashboard</h1>
+            <p className="text-lg mb-4">Please sign in to access the dashboard</p>
+          </div>
+        </div>
+      </SignedOut>
     </>
   );
 }
