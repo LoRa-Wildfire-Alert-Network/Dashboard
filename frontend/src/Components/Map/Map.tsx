6660f086519815a4c2fdde073e3d2758404b7890
Derek Knowlton <knowltod@oregonstate.edu>
2026-01-24 22:07:49 -0800

    cleaned up import for Map and NodeCard

--- frontend/src/Components/Map/Map.tsx
+++ frontend/src/Components/Map/Map.tsx
@@ -1,6 +1,6 @@
 import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import type { NodeData } from "../../types/nodeTypes";
 
 interface MapProps {
--- frontend/src/Components/NodeCardList/NodeCard.tsx
+++ frontend/src/Components/NodeCardList/NodeCard.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import type { NodeData } from "../../types/nodeTypes";
-import { useState } from "react";
 import CardLongData from "./CardLongData";
 import CardShortData from "./CardShortData";
 
