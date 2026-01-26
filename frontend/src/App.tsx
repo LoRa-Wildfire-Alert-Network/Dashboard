7dd5448180abddbac547b4c862306734283be459
Derek Knowlton <knowltod@oregonstate.edu>
2026-01-25 21:13:45 -0800

    sample change of a BG color to test rebuild functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <div className="flex space-x-4 w-full h-full p-4">
             <div className="flex-none lg:w-80 md:w-48 bg-slate-200 rounded-md p-4"></div>
             <Map nodeData={testData} />
-            <div className="flex-none overflow-y-auto lg:w-100 md:w-60 bg-slate-400 rounded-md py-2 px-4">
+            <div className="flex-none overflow-y-auto lg:w-100 md:w-60 bg-red-400 rounded-md py-2 px-4">
               {testData.map((nodeData, i) => (
                 <NodeCard key={i} nodeData={nodeData} />
               ))}
@@ -27,8 +27,12 @@ function App() {
       <SignedOut>
         <div className="bg-slate-300 h-[calc(100vh-4rem)] flex items-center justify-center">
           <div className="text-center">
-            <h1 className="text-2xl font-semibold mb-4">Check it out, even more changes!</h1>
-            <p className="text-lg mb-4">Please sign in to access the dashboard</p>
+            <h1 className="text-2xl font-semibold mb-4">
+              Check it out, even more changes!
+            </h1>
+            <p className="text-lg mb-4">
+              Please sign in to access the dashboard
+            </p>
           </div>
         </div>
       </SignedOut>
