5db88d32c6e4a4ed7b5bbc0c45ea446f5598de62
Derek R. Greene <derek@derekrgreene.com>
2026-03-08 00:24:31 -0800

    :recycle: Refactor: Vite config for PROD

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,17 +3,24 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const DOCS_PORT = process.env.DOCS_PORT || 4000
+// in PROD mode, serve /docs from dist/docs/ (no proxy)
+const isPreview = process.env.VITE_PREVIEW === '1'
 
 // https://vite.dev/config/
 export default defineConfig({
+  preview: {
+    allowedHosts: ['.derekrgreene.com', 'localhost', '127.0.0.1'],
+  },
   server: {
-    proxy: {
-      '/docs': {
-        target: `http://localhost:${DOCS_PORT}`,
-        changeOrigin: true,
-        rewrite: (path) => (path === '/docs' ? '/docs/' : path),
-      },
-    },
+    proxy: isPreview
+      ? undefined
+      : {
+          '/docs': {
+            target: `http://localhost:${DOCS_PORT}`,
+            changeOrigin: true,
+            rewrite: (path) => (path === '/docs' ? '/docs/' : path),
+          },
+        },
   },
   plugins: [
     react({
